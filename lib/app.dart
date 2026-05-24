d396362f04dc9e1abf1aa3d92b75bb1e5e649775
johnadelm23-beep <https://github.com/johnadelm23-beep/bmi_calculator>
2026-05-24 22:14:14 +0300

    add n

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:programmin/features/auth/ui/login_screen.dart';
+import 'package:programmin/features/auth/ui/register_screen.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: LoginScreen());
+  }
+}
